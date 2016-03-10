57c26651763f5c9a0983e1d6e8c0e6317f0bd702
Mark Fletcher <mook.fletcher@gmail.com>
2016-03-10 18:18:35 -0500

    first layout iteration complete, just requires personal info filling out

--- app/assets/javascripts/info_selector.js
+++ app/assets/javascripts/info_selector.js
@@ -4,7 +4,8 @@ $(function() {
     $('.info').hide();
     $('.' + this["id"]).toggle();
 
-    // $(this).addClass('.selected')
-    // do the selected highlighting next
+    $('.infolink').removeClass('selected')
+    $(this).addClass('selected')
+
   })
 });
